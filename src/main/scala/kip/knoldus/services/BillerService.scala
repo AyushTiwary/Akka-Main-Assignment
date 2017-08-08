a67efd13e2cacb2eec833d067ae08081dad5a06c
ayush <ayush.tiwari@knoldus.in>
2017-08-08 14:44:37 +0530

    modified BillerService

--- src/main/scala/kip/knoldus/services/BillerService.scala
+++ src/main/scala/kip/knoldus/services/BillerService.scala
@@ -1,15 +1,18 @@
 package kip.knoldus.services
 
 import kip.knoldus.Database
-import kip.knoldus.Database.database
+import kip.knoldus.Database.{accountInfo, database}
 import kip.knoldus.models.{Account, Biller}
 
 class BillerService {
 
   def payAmt(account: Account, biller: Biller): database.type = {
 
+    Database.accountInfo += (account.username -> account.copy(initialAmount = account.initialAmount - biller.amountPaid))
+
     Database.database += (account.accountNumber -> (account.copy(initialAmount = account.initialAmount - biller.amountPaid),
       List(biller.copy(amountPaid = biller.amountPaid, totalPaidAmount = biller.totalPaidAmount + biller.amountPaid))))
+
   }
 
 }
